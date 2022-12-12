1231f0cb367d8f57e6588da4b8127b7b263182e4
tacacho <80930736+tacacho@users.noreply.github.com>
2022-12-12 23:51:12 +0000

    Delete Automation_Custom_Script.sh

--- Automation_Custom_Script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This file is executed after the successful installation of DietPi OS.
-# My custom install happens.
-
-# Install i3, i3lock, i3status, and suckless-tools
-echo "Installing extras..."
-apt-get install i3 i3lock i3status suckless-tools ranger lynx btop -y
