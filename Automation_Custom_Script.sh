eee46421cbd4e4e253c7f2d73ebec921c78c7cd5
tacacho <80930736+tacacho@users.noreply.github.com>
2022-12-12 23:41:54 +0000

    Add files via upload

---
+++ Automation_Custom_Script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This file is executed after the successful installation of DietPi OS.
+# Tom's custom install happens.
+
+# Install i3, i3lock, i3status, and suckless-tools
+echo "Installing extras..."
+apt-get install i3 i3lock i3status suckless-tools ranger lynx btop -y
